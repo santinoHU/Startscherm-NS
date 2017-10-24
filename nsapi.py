7933ea7593bad2748217a9bd3269dbd0fa887ad9
Santino den Brave <santino.denbrave@student.hu.nl>
2017-10-24 11:33:58 +0200

    tkinter opzet

--- nsapi.py
+++ nsapi.py
@@ -1,18 +1,28 @@
 import requests
 import xmltodict
+from tkinter import *
 
-auth_details = ('santino.denbrave@student.hu.nl', 'P_v4mmnZiBdVPeEOPsIzVbggnwm1T9EPYPwT0yDZX6vo7Q5rK3VLpw')
-api_url = 'http://webservices.ns.nl/ns-api-avt?station='
-response = requests.get(api_url, auth=auth_details)
-# print(response.text)
+root = Tk()
 
-vertrekXML = xmltodict.parse(response.text)
+label = Label(master=root, text="Ik wil naar \n Amsterdam", background="blue")
 
-print('Dit zijn de vertrekkende treinen:')
-for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
- eindbestemming = vertrek['EindBestemming']
- vertrektijd = vertrek['VertrekTijd'] # 2016-09-27T18:36:00+0200
- vertrektijd = vertrektijd[11:16] # 18:36
- print('Om '+ vertrektijd+' vertrekt een trein naar '+ eindbestemming)
+label.pack()
+
+root.mainloop()
+
+
+# auth_details = ('santino.denbrave@student.hu.nl', 'P_v4mmnZiBdVPeEOPsIzVbggnwm1T9EPYPwT0yDZX6vo7Q5rK3VLpw')
+# api_url = 'http://webservices.ns.nl/ns-api-avt?station=alphen'
+# response = requests.get(api_url, auth=auth_details)
+# # print(response.text)
+#
+# vertrekXML = xmltodict.parse(response.text)
+#
+# print('Dit zijn de vertrekkende treinen:')
+# for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+#  eindbestemming = vertrek['EindBestemming']
+#  vertrektijd = vertrek['VertrekTijd'] # 2016-09-27T18:36:00+0200
+#  vertrektijd = vertrektijd[11:16] # 18:36
+#  print('Om '+ vertrektijd+' vertrekt een trein naar '+ eindbestemming)
 # with open('vertrektijden.xml', 'w') as myXMLFile:
 #  myXMLFile.write(response.text)
