48e1e6b341b1ee4c0c245c236a8c2f6d380ee9a4
Santino den Brave <santino.denbrave@student.hu.nl>
2017-11-01 17:30:41 +0100

    Tweede scherm aangepast. Extra zoekveld meegegeven en standaardstation ingesteld. Zoekfunctie verbeterd en READ.ME gemaakt

--- nsapi.py
+++ nsapi.py
@@ -11,49 +11,14 @@ fg = "#013097" # blue
 # creates GUI
 root = Tk()
 root.title("Welkom bij de NS")
-root.geometry("450x533")
+root.geometry("450x550")
 root.configure(background=bg)
 
-# function that opens the search GUI
-def command():
-    top = Toplevel(root)
-    top.title("Plan uw reis")
-    top.configure(background=bg)
-    top.geometry("450x533")
-
-    # function to check search results
-    def search():
-        # enables the use of the ns-api
-        auth_details = ('santino.denbrave@student.hu.nl', 'P_v4mmnZiBdVPeEOPsIzVbggnwm1T9EPYPwT0yDZX6vo7Q5rK3VLpw')
-        api_url = 'http://webservices.ns.nl/ns-api-avt?station=' + entry.get()
-        response = requests.get(api_url, auth=auth_details)
-
-        # returns search-information
-        vertrekXML = xmltodict.parse(response.text)
-        for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-            eindbestemming = vertrek['EindBestemming']
-            vertrektijd = vertrek['VertrekTijd']  # 2016-09-27T18:36:00+0200
-            vertrektijd = vertrektijd[11:16]  # 18:36
-            results["text"] = 'Om ' + vertrektijd + ' vertrekt een trein naar ' + eindbestemming
-        subtitle.pack(pady=(40, 0))
-        results.pack()
-
-    # assign variables
-    entry = Entry(top)
-    description = Label(top, text="Voer hieronder station in waarvan u informatie wilt", foreground=fg, background=bg, font=('Helvetica'))
-    searchbtn = Button(top, text="Zoeken", command=search)
-    title = Label(top, text="Welkom bij NS", foreground=fg, background=bg, font=('Helvetica', 22, 'bold'))
-    subtitle = Label(top, text="Dit zijn de vertrekkende treinen:\n", background=bg, font=('Helvetica', 16, 'bold'))
-    results = Label(top, text="", background=bg)
-
-    # prints the variables to the GUI
-    title.pack()
-    description.pack(pady=(40,0)) #pady works as a tuple: (top, bottom)
-    entry.pack(padx=10, pady=10)
-    searchbtn.pack()
+# ns-api auth
+api_url = 'http://webservices.ns.nl/ns-api-avt?station='
+auth_details = ('santino.denbrave@student.hu.nl', 'P_v4mmnZiBdVPeEOPsIzVbggnwm1T9EPYPwT0yDZX6vo7Q5rK3VLpw')
 
 # assign buttons
-searchbtn = Button(master = root, text="Reis \nplannen", foreground="white", background=fg, command=command)
 ticketbtn = Button(master = root, text="Kopen \n los kaartje", foreground="white", background=fg)
 cardbtn = Button(master = root, text="Kopen \n OV-Chipkaart", foreground="white", background=fg)
 abroadbtn = Button(master = root, text="Ik wil naar \n het buitenland", foreground="white", background=fg)
@@ -67,15 +32,75 @@ placeholder = Label(master = root, background=bg)
 title = Label(text="Welkom bij NS", foreground="#002272", background=bg, font=('Helvetica', 22, 'bold'))
 panel = Label(root, image = img, background=bg)
 
+# function that opens the search GUI
+def command():
+    top = Toplevel(root)
+    top.title("Plan uw reis")
+    top.configure(background=bg)
+    top.geometry("450x550")
+
+    # function to check search results
+    def search():
+        search_url = api_url + entry_from.get()
+        # sets the default to alphen if there is no input
+        if search_url == 'http://webservices.ns.nl/ns-api-avt?station=':
+            search_url = 'http://webservices.ns.nl/ns-api-avt?station=alphen'
+        response = requests.get(search_url, auth=auth_details)
+        # returns search-information
+        vertrekXML = xmltodict.parse(response.text)
+        count = 0
+        # subtitle.pack(pady=(20, 0))
+        for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+            # displays a maximum of 10 trains on the screen
+            if count <= 9:
+                eindbestemming = vertrek['EindBestemming']
+                vertrektijd = vertrek['VertrekTijd']  # 2016-09-27T18:36:00+0200
+                vertrektijd = vertrektijd[11:16]  # 18:36
+                vertrekspoor = vertrek['VertrekSpoor'] # returns ordered dict
+                trein = vertrek['TreinSoort']
+                results = Label(top, text="", background=bg, foreground=fg, font=("bold"))
+                # if entry_to.get() in eindbestemming.lower() and opmerking != "":
+                #     results["text"] = "Om " + vertrektijd + " vertrekt de " + trein + " naar " + eindbestemming + opmerking
+                #     results.pack()
+                # el
+                if entry_to.get() in eindbestemming.lower():
+                    results["text"] = "Om " + vertrektijd + " vertrekt de " + trein + " naar " + eindbestemming
+                    results.pack()
+                count += 1
+
+    # assign variables
+    entry_from = Entry(top)
+    from_txt = Label(top, text="van*", foreground=fg, background=bg, font=('Helvetica'))
+    entry_to = Entry(top)
+    to_txt = Label(top, text="naar", foreground=fg, background=bg, font=('Helvetica'))
+    description = Label(top, text="Of zoek uw reis handmatig:", foreground=fg, background=bg, font=('Helvetica'))
+    footer2 = Label(master=top, background=fg)
+    searchbttn = Button(top, text="Zoeken", command=search)
+    currentbttn = Button(top, text="Huidige stationsinformatie", command=search)
+    title = Label(top, text="Welkom bij NS", foreground=fg, background=bg, font=('Helvetica', 22, 'bold'))
+    # subtitle = Label(top, text="Dit zijn de vertrekkende treinen:\n", foreground=fg, background=bg, font=('Helvetica'))
+
+    # prints the variables to the GUI
+    title.pack(pady=(20,0)) # pady works as a tuple: (top, bottom)
+    currentbttn.pack(pady=(10,0))
+    description.pack(pady=(10,0))
+    from_txt.pack()
+    entry_from.pack(padx=10, pady=0)
+    entry_from.focus() # allows you to type in this inputfield without clicking first
+    to_txt.pack()
+    entry_to.pack(padx=10, pady=0)
+    searchbttn.pack(pady=(15,25))
+    footer2.pack(side=BOTTOM, fill=X)
+
 # shows variables on the GUI
 title.pack(pady=20)
 panel.pack(padx=10)
 placeholder.pack(side=LEFT, padx=20)
-searchbtn.pack(side=LEFT, padx=5, pady=80)
+searchbtn = Button(master = root, text="Reis \nplannen", foreground="white", background=fg, command=command).pack(side=LEFT, padx=5, pady=80)
 ticketbtn.pack(side=LEFT, padx=5, pady=80)
 cardbtn.pack(side=LEFT, padx=5, pady=80)
 abroadbtn.pack(side=LEFT, padx=5, pady=80)
-footer.place(x=0, y=513, width=650)
+footer.place(x=0, y=530, width=650)
 
 # open GUI
 root.mainloop()
