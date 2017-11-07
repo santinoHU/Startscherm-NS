d76964dc0489a13b2116365270990e2ad989b70e
Youri <youri@demuldertjes.nl>
2017-11-07 11:28:28 +0100

    Response gedecode bij elke request, breedte van de gui aangepast, reisinformatie gemaakt/aangepast, een bug bij VEDELING.ME gemeld

--- nsapi.py
+++ nsapi.py
@@ -8,16 +8,33 @@ from PIL import ImageTk, Image
 from auth import *
 
 
+
+
+def check_route_stations(vertrek, eind, stations):
+    if vertrek not in stations:
+        messagebox.showinfo("Error", "Het vertrekstation bestaat niet")
+        return False
+    if eind not in stations:
+        messagebox.showinfo("Error", "Het eindstation bestaat niet")
+        return False
+    if vertrek == eind:
+        messagebox.showinfo("Error", "U kunt niet naar hetzelfde station reizen")
+        return False
+
+
 # all stations to a tuple
 def allstations():
     stationlist = []
     stations_url = "http://webservices.ns.nl/ns-api-stations"
+
+    # get response and decode it UTF-8
     response = requests.get(stations_url, auth=auth_details)
+    response.encoding = 'UTF-8'
+
     stationsXML = xmltodict.parse(response.text)
     for station in stationsXML['stations']['station']:
         stationlist.append(station['name'])
     return tuple(stationlist)
-
 
 # all NS staions
 stations = allstations()
@@ -30,7 +47,7 @@ fg = "#013097" # blue
 # creates home GUI
 root = Tk()
 root.title("Welkom bij de NS")
-root.geometry("450x550")
+root.geometry("900x550")
 root.resizable(width=False, height=False)
 root.configure(background=bg)
 
@@ -43,10 +60,10 @@ abroadbtn = Button(master = root, text="Ik wil naar \n het buitenland", foregrou
 img = ImageTk.PhotoImage(Image.open("img/ns_img.png"))
 
 # assign labels
-footer = Label(master = root, background=fg)
-placeholder = Label(master = root, background=bg)
+footer = Label(master=root, background=fg)
+placeholder = Label(master=root, background=bg)
 title = Label(text="Welkom bij NS", foreground="#002272", background=bg, font=('Helvetica', 22, 'bold'))
-panel = Label(root, image = img, background=bg)
+panel = Label(root, image=img, background=bg)
 
 
 # function that opens the search GUI
@@ -55,7 +72,7 @@ def command():
     top = Toplevel(root)
     top.title("Plan uw reis")
     top.configure(background=bg)
-    top.geometry("450x550")
+    top.geometry("900x550")
     top.resizable(width=False, height=False)
     top.withdraw()
 
@@ -71,7 +88,7 @@ def command():
         results.delete('0.0', END)
 
         # getting formatted entry from the combobox
-        vertrekstation = str(entry_from.get()).replace(' ', '+')
+        vertrekstation = str(entry_from.get().replace(' ', '+'))
 
 
         search_url = api_url_station + vertrekstation
@@ -80,13 +97,20 @@ def command():
         if search_url == 'http://webservices.ns.nl/ns-api-avt?station=':
             search_url = 'http://webservices.ns.nl/ns-api-avt?station=alphen'
 
+        # get response and decode it UTF-8
         response = requests.get(search_url, auth=auth_details)
+        response.encoding = 'UTF-8'
+
         vertrekXML = xmltodict.parse(response.text)
 
         # checks if station is foreign
         try:
             if vertrekXML['error']['message'] == 'Foreign stations are not supported.':
                 messagebox.showinfo("Error", "Buitenlandse stations kunt u hiervoor niet gebruiken.")
+                return
+            elif vertrekXML['error']['message'].startswith("Error while trying to get departure information for station"):
+                messagebox.showinfo("Error", "Op het moment kunnen wij niet de data van NS verkrijgen.\n"
+                                             "probeer later nog eens!")
                 return
         except:
             pass
@@ -99,7 +123,7 @@ def command():
             trein = vertrek['TreinSoort']
 
             if vertrekspoor["@wijziging"] == 'true':
-                results.insert(0.0, "Om " + vertrektijd + " vertrekt de " + trein + " naar " + eindbestemming + vertrekspoor['#text'] + "\n !Let op: Spoorswijziging")
+                results.insert(0.0, "Om " + vertrektijd + " vertrekt de " + trein + " naar " + eindbestemming + vertrekspoor['#text'] + "\n !Let op: Spoorswijziging\n")
             else:
                 results.insert(0.0, "Om " + vertrektijd + " vertrekt de " + trein + " naar " + eindbestemming + " vanaf spoor " + vertrekspoor['#text'] + "\n")
 
@@ -113,10 +137,10 @@ def command():
 
         # check if both fields are filled in
         if entry_from.get() is "":
-            messagebox.showinfo("Error", "Zorg ervoor dat het beginstation is ingevuld.")
+            messagebox.showinfo("Error", "Zorg ervoor dat het beginstation is ingevuld")
             return
         if entry_to.get() is "":
-            messagebox.showinfo("Error", "Zorg ervoor dat het eindstation is ingevuld.")
+            messagebox.showinfo("Error", "Zorg ervoor dat het eindstation is ingevuld")
             return
 
 
@@ -124,33 +148,63 @@ def command():
         vertrekstation = 'fromStation=' + str(entry_from.get()).replace(' ', '+')
         eindstation = 'toStation=' + str(entry_to.get()).replace(' ', '+')
 
+        # checks if is input is valid
+        if check_route_stations(entry_from.get(), entry_to.get(), stations) == False:
+            return
+
+
 
         search_url = api_url_route + vertrekstation + '&' + eindstation
         # sets the default to alphen if there is no input
         if search_url == 'http://webservices.ns.nl/ns-api-treinplanner?':
             search_url = 'http://webservices.ns.nl/ns-api-treinplanner?fromStation=Utrecht+Centraal&toStation=Wierden'
+
+        # get response and decode it UTF-8
         response = requests.get(search_url, auth=auth_details)
+        response.encoding = 'UTF-8'
+
         vertrekXML = xmltodict.parse(response.text)
-        print(vertrekXML)
+
+        # make a table to display
+        results.insert(0.0, "{:25} {:21} {:25} {:21} \n".format("Vertrekstation", "Vertrektijd", "Eindstation", "Aankomsttijd"))
+
+        # checks if route is possible
+        if vertrekXML['ReisMogelijkheden'] is None:
+            messagebox.showinfo("Error", "Deze reis is niet mogelijk probeer andere stations")
+            return
 
 
         for vertrek in vertrekXML['ReisMogelijkheden']['ReisMogelijkheid']:
             print(vertrek)
+            melding = False
 
+            # assigns variables when there is a adjusted schedule
+            try:
+                id = vertrek['Melding']['Id']
+                ernstig = vertrek['Melding']['Ernstig']
+                text = vertrek['Melding']['Text']
+                melding = True
+            except:
+                print("Exception raised when assiging routeinformation variables")
 
-            # eindbestemming = vertrek['EindBestemming']
-            # vertrektijd = vertrek['VertrekTijd']  # 2016-09-27T18:36:00+0200
-            # vertrektijd = vertrektijd[11:16]  # 18:36
-            # vertrekspoor = vertrek['VertrekSpoor']  # returns ordered dict
-            # trein = vertrek['TreinSoort']
-            #
-            # if entry_to.get() in eindbestemming.lower() and count <= 9:
-            #     count += 1
-            #     results.insert(0.0, "Om " + vertrektijd + " vertrekt de " + trein + " naar " + eindbestemming + " vanaf spoor " + vertrekspoor['#text'] + "\n")
-            #     if vertrekspoor["@wijziging"] == 'true':
-            #         results.insert(0.0, "Om " + vertrektijd + " vertrekt de " + trein + " naar " + eindbestemming + vertrekspoor['#text'] + "\n !Let op: Spoorswijziging")
-            #
-            # results.pack(side=LEFT, fill='both', expand=YES, padx=5, pady=5)
+            try:
+                aantaloverstappen = vertrek['AantalOverstappen']
+                geplandereistijd = vertrek['GeplandeReisTijd']  # 2016-09-27T18:36:00+0200
+                actuelereistijd = vertrek['ActueleReisTijd']  # 2016-09-27T18:36:00+0200
+                optimaal = vertrek['Optimaal']
+                geplandevertrektijd = vertrek['GeplandeVertrekTijd']  # 2016-09-27T18:36:00+0200
+                actuelevertrektijd = vertrek['ActueleVertrekTijd']  # 2016-09-27T18:36:00+0200
+                geplandeaankomsttijd = vertrek['GeplandeAankomstTijd']  # 2016-09-27T18:36:00+0200
+                actueleaankomsttijd = vertrek['ActueleAankomstTijd']  # 2016-09-27T18:36:00+0200
+                status = vertrek['Status']
+            except:
+                print("Exception raised when assiging routeinformation variables")
+
+            if melding == True:
+                results.insert(END, "PAS OP! De volgende reis heeft een melding: " + text + "\n")
+            results.insert(END, "{:24} {:5} {:8} {:4} {:24} {:5} {:8} {:4} \n".format(entry_from.get() , actuelevertrektijd[5:10], actuelevertrektijd[11:16], "|", entry_to.get(), actueleaankomsttijd[5:10], actueleaankomsttijd[11:16]))
+
+            results.pack(side=LEFT, fill='both', expand=YES, padx=5, pady=5)
 
 
 
@@ -197,12 +251,12 @@ def command():
 # shows variables on the GUI
 title.pack(pady=20)
 panel.pack(padx=10)
-placeholder.pack(side=LEFT, padx=20)
-searchbtn = Button(master = root, text="Reis \nplannen", foreground="white", background=fg, command=command).pack(side=LEFT, padx=5, pady=80)
+placeholder.pack(side=LEFT, padx=140)
+searchbtn = Button(master=root, text="Reis \nplannen", foreground="white", background=fg, command=command).pack(side=LEFT, padx=5, pady=80)
 ticketbtn.pack(side=LEFT, padx=5, pady=80)
 cardbtn.pack(side=LEFT, padx=5, pady=80)
 abroadbtn.pack(side=LEFT, padx=5, pady=80)
-footer.place(x=0, y=530, width=650)
+footer.place(x=0, y=530, width=root.winfo_screenwidth())
 
 # open GUI
 root.mainloop()
